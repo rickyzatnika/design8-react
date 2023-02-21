320fa70265dcf7b19369ae597ed725766db68e94
rickyzatnika <rickyzatnika91@gmail.com>
2023-02-21 14:51:42 +0700

    hapus lightgallery

--- src/components/Invitations/SectionFive.jsx
+++ src/components/Invitations/SectionFive.jsx
@@ -1,65 +1,59 @@
 import React from "react";
-import LightGallery from "lightgallery/react";
-
-import "lightgallery/css/lightgallery.css";
-import "lightgallery/css/lg-zoom.css";
-import "lightgallery/css/lg-thumbnail.css";
-
-import lgThumbnail from "lightgallery/plugins/thumbnail";
-import lgZoom from "lightgallery/plugins/zoom";
+import baguetteBox from "baguettebox.js";
+import "baguettebox.js/dist/baguetteBox.min.css";
 
 const SectionFive = () => {
+  baguetteBox.run(".gallery", {
+    animation: "fadeIn",
+    buttons: true,
+    noScrollbars: true,
+    overlayBackgroundColor: "black",
+  });
   return (
     <>
-      <div className="w-full h-full py-0 lg:py-10">
-        <LightGallery
-          speed={500}
-          elementClassNames="w-full h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1 px-2 md:px-10 lg:px-20 "
-          plugins={[lgThumbnail, lgZoom]}
-        >
-          <a href="/images/image-1.jpg">
-            <img
-              alt="img1"
-              src="/images/image-1.jpg"
-              className="w-full object-cover"
-            />
-          </a>
-          <a href="/images/image-2.jpg">
-            <img
-              alt="img1"
-              src="/images/image-2.jpg"
-              className="w-full object-cover"
-            />
-          </a>
-          <a href="/images/image-3.jpg">
-            <img
-              alt="img1"
-              src="/images/image-3.jpg"
-              className="w-full object-cover"
-            />
-          </a>
-          <a href="/images/image-8.jpg">
-            <img
-              alt="img1"
-              src="/images/image-8.jpg"
-              className="w-full object-cover"
-            />
-          </a>
-          <a href="/images/image-4.jpg">
-            <img
-              alt="img1"
-              src="/images/image-4.jpg"
-              className="w-full object-cover"
-            />
-          </a>
-          <a href="/images/image-5.jpg">
-            <img
-              alt="img1"
-              src="/images/image-5.jpg"
-              className="w-full object-cover"
-            />
-          </a>
-        </LightGallery>
+      <div className="gallery grid grid-cols-3 w-full h-full py-0 lg:py-10">
+        <a href="/images/image-1.jpg" data-caption="Image caption">
+          <img
+            alt="img1"
+            src="/images/image-1.jpg"
+            className="w-full object-cover"
+          />
+        </a>
+        <a href="/images/image-2.jpg">
+          <img
+            alt="img1"
+            src="/images/image-2.jpg"
+            className="w-full object-cover"
+          />
+        </a>
+        <a href="/images/image-3.jpg">
+          <img
+            alt="img1"
+            src="/images/image-3.jpg"
+            className="w-full object-cover"
+          />
+        </a>
+        <a href="/images/image-8.jpg">
+          <img
+            alt="img1"
+            src="/images/image-8.jpg"
+            className="w-full object-cover"
+          />
+        </a>
+        <a href="/images/image-4.jpg">
+          <img
+            alt="img1"
+            src="/images/image-4.jpg"
+            className="w-full object-cover"
+          />
+        </a>
+        <a href="/images/image-5.jpg">
+          <img
+            alt="img1"
+            src="/images/image-5.jpg"
+            className="w-full object-cover"
+          />
+        </a>
       </div>
     </>
   );
